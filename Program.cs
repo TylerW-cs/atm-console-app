8549000b16d677e836c6cfe1f40fa833de2ea8c1
Tyler Wood <tylerwood1701@outlook.com>
2026-09-24 21:52:58 +0100

    Added comments

--- Program.cs
+++ Program.cs
@@ -12,26 +12,26 @@ do // Start of the do-while loop
     {
         case 1: // If input is 1.
         Console.WriteLine($"\nYour balance is: £{balance:F2}\n"); // Outputs the users balance amount.
-        break; // Ends the first case.
+        break; // Exits the switch statement.
             
         case 2: // If input is 2.
         Console.WriteLine("\nHow much would you like to deposit?"); // Outputs asking how much the user wants to deposit.
         depositAmount = decimal.Parse(Console.ReadLine()); // Assigning the input the user inputs (which is converted to a decimal data type) to the variable called depositAmount.
         balance += depositAmount; // Balance gets updated to add on the depositAmount the user inputted.
         Console.WriteLine($"\nYour new balance is: £{balance:F2}\n"); // Outputs the updated balance amount of the user, making sure it is outputted to 2 decimal points.
-        break; // Ends the second case.
+        break; // Exits the switch statement.
 
         case 3: // If input is 3.
         Console.WriteLine("\nHow much would you like to withdraw?"); // Ouputes asking how much the user wants to withdraw.
         withdrawAmount = decimal.Parse(Console.ReadLine()); // Assigning the input the user inputs (which is converted to a decimal data type) to the variable called withdrawAmount.
         string withdrawLimit = withdrawAmount > balance // Start of the Ternary Operator. 
         ? "\nSorry you do not have enough to make this withdrawal.\n" // If the withdrawAmount is more than the balance then this will be outputted to the console at the end of this case.
-        : $"\nYour new balance is: £{(balance -= withdrawAmount):F2}\n"; // If the withdrawAmount is less or equal to the balance then this will be outputted to the console at the end of this case.
-        Console.WriteLine("\n" + withdrawLimit); // Ouputs the relevant answer from above.
-        break; // Ends the third case.
+        : $"\nYour new balance is: £{(balance -= withdrawAmount):F2}\n"; // If the withdrawAmount is less or equal to the balance then the withdraw amount gets removed from the balance and the new balance to 2 decimal places and is outputted to the console at the end of this case.
+        Console.WriteLine(withdrawLimit); // Ouputs the relevant answer from above.
+        break; // Exits the switch statement.
 
         case 4: // If input is 4.
         Console.WriteLine("\nGoodbye!\n"); // Ouputs a goodbye message.
-        break; // Ends the fourth case.
+        break; // Exits the switch statement.
     }
 } while (option != 4); // End of the do-while loop. Does not end unless the input integer is 4.
